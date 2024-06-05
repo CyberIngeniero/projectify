c81ef44f6295cd5878f5a1f271fd9d9cb5bccbba
Nibaldo Pino A <npino@uc.cl>
2024-06-05 16:58:54 +0200

    Bump version: 0.1.1 → 0.1.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="Projectify",
-    version="0.1.1",
+    version="0.1.2",
     description="A tool to set up a Python project structure",
     author="CyberIngeniero",
     author_email="npinoa.ai@gmail.com",
